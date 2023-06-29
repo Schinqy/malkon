7f1c33b736d44d4aabd346d3029750eda24f3de2
Schinqy <101190248+Schinqy@users.noreply.github.com>
2023-06-29 18:36:06 +0200

    Add files via upload

---
+++ variants/malkon_esp32s3/pins_arduino.h
@@ -0,0 +1,90 @@
+//MALKON TWO (ESP32S3) PIN DEFINITION
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+#include "soc/soc_caps.h"
+
+#define USB_VID 0x303a
+#define USB_PID 0x1001
+#define USB_MANUFACTURER   "Malkon "
+#define USB_PRODUCT        "Two ESP32-S3"
+#define USB_SERIAL         "" // Empty string for MAC adddress
+
+#define EXTERNAL_NUM_INTERRUPTS 46
+#define NUM_DIGITAL_PINS        48
+#define NUM_ANALOG_INPUTS       20
+
+// Some boards have too low voltage on this pin (board design bug)
+// Use different pin with 3V and connect with 48
+// and change this setup for the chosen pin (for example 38)
+static const uint8_t LED_BUILTIN = 13;
+#define BUILTIN_LED  LED_BUILTIN // backward compatibility
+#define LED_BUILTIN LED_BUILTIN
+#define RGB_BRIGHTNESS 64
+
+#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
+#define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
+#define digitalPinHasPWM(p)         (p < 46)
+
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
+
+static const uint8_t SDA = 14;
+static const uint8_t SCL = 8;
+
+static const uint8_t SS    = 10;
+static const uint8_t MOSI  = 35;
+static const uint8_t MISO  = 37;
+static const uint8_t SCK   = 36;
+
+static const uint8_t A0 = 18;
+static const uint8_t A1 = 17;
+static const uint8_t A2 = 16;
+static const uint8_t A3 = 15;
+static const uint8_t A4 = 14;
+static const uint8_t A5 = 8;
+
+
+static const uint8_t D0 = 43;
+static const uint8_t D1 = 44;
+static const uint8_t D2 = 2;
+static const uint8_t D3 = 21;
+static const uint8_t D4 = 4;
+static const uint8_t D5 = 5;
+static const uint8_t D6 = 6;
+static const uint8_t D7 = 7;
+static const uint8_t D8 = 38;
+static const uint8_t D9 = 9;
+static const uint8_t D10 = 10;
+static const uint8_t D11 = 11;
+static const uint8_t D12 = 12;
+static const uint8_t D13 = 13;
+
+
+static const uint8_t CSN = 41;
+static const uint8_t CE = 42;
+
+
+static const uint8_t USBN = 19;
+static const uint8_t USBP= 20;
+
+
+static const uint8_t T1 = 1;
+static const uint8_t T2 = 2;
+static const uint8_t T3 = 3;
+static const uint8_t T4 = 4;
+static const uint8_t T5 = 5;
+static const uint8_t T6 = 6;
+static const uint8_t T7 = 7;
+static const uint8_t T8 = 8;
+static const uint8_t T9 = 9;
+static const uint8_t T10 = 10;
+static const uint8_t T11 = 11;
+static const uint8_t T12 = 12;
+static const uint8_t T13 = 13;
+static const uint8_t T14 = 14;
+
+
+
+#endif /* Pins_Arduino_h */
